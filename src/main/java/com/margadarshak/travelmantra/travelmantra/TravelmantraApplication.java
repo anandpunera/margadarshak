fcdf0594e399a7e974edd8b761d5ebe09d6133ef
anandpunera <anand.c.punera@gmail.com>
2018-10-21 19:23:56 +0530

    First commit

---
+++ src/main/java/com/margadarshak/travelmantra/travelmantra/TravelmantraApplication.java
@@ -0,0 +1,12 @@
+package com.margadarshak.travelmantra.travelmantra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmantraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmantraApplication.class, args);
+	}
+}
