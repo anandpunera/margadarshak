083e984f61fbf31c6a79227e18fe481ca73a34e5
anandpunera <anand.c.punera@gmail.com>
2018-10-22 21:01:37 +0530

    Remove sample project files

--- src/main/java/com/margadarshak/travelmantra/margadarshak/UserMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.margadarshak.travelmantra.margadarshak;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<User, Integer> {
-    User findByName(String name);
-}
